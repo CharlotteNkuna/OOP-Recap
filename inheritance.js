0a465a0bfbe735cf20bac0537cc3de19fadcdb29
Charlotte <cnkuna920@gmail.com>
2025-08-12 11:41:57 +0200

    changes made

--- inheritance.js
+++ inheritance.js
@@ -14,3 +14,29 @@ extends Animal {
     } 
 } 
 const dog = new Dog("Rex"); dog.speak();
+
+
+
+//using keywords        
+class toy{
+    constructor(name, color){
+        this.name = name;
+        this.color = color;
+    }
+    showDetails(){
+        return`${this.name} is ${this.color}`;
+    }
+}
+
+class doll extends toy{
+    constructor(name, color, dressStyle){
+        super(name, color); //inherit constructors from parent class
+        this.dressStyle = dressStyle;
+    }
+
+    getToys(){
+        return`This toy is a ${this.name} doll and is ${this.color}. It is dressed in a ${this.dressStyle}.`;
+    }
+}
+let doll1 = new doll("Barbie", "nude", "princess dress");
+console.log(doll1.getToys());
